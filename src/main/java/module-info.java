99ca4f97bb499e57a2345761603c624b19495d53
Gurkan UCAR <ucar.gurkan@hotmail.com>
2024-01-09 20:14:17 +0300

    feat: logger implemented

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,9 +1,14 @@
 module com.gucardev.apitestautomation {
     requires javafx.controls;
     requires javafx.fxml;
-            
-                        requires org.kordamp.bootstrapfx.core;
-            
+
+    requires org.kordamp.bootstrapfx.core;
+    requires lombok;
+    requires org.apache.poi.poi;
+
+    requires org.slf4j;
+    requires org.apache.poi.ooxml;
+
     opens com.gucardev.apitestautomation to javafx.fxml;
     exports com.gucardev.apitestautomation;
 }
