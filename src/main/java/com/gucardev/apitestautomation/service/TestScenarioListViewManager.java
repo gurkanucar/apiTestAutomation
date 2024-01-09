2f8aac77a11d359688d14e3be50b8786d031be64
Gurkan UCAR <ucar.gurkan@hotmail.com>
2024-01-09 20:56:40 +0300

    feat: test scenarios details displayed in alert dialog

--- src/main/java/com/gucardev/apitestautomation/service/TestScenarioListViewManager.java
+++ src/main/java/com/gucardev/apitestautomation/service/TestScenarioListViewManager.java
@@ -3,9 +3,11 @@ package com.gucardev.apitestautomation.service;
 import com.gucardev.apitestautomation.model.TestScenario;
 import java.util.List;
 import javafx.application.Platform;
+import javafx.scene.control.Alert;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
-import javafx.util.Callback;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
 
 public class TestScenarioListViewManager {
 
@@ -18,10 +20,8 @@ public class TestScenarioListViewManager {
 
   private void configureListView() {
     listView.setCellFactory(
-        new Callback<>() {
-          @Override
-          public ListCell<TestScenario> call(ListView<TestScenario> lv) {
-            return new ListCell<>() {
+        lv ->
+            new ListCell<>() {
               @Override
               protected void updateItem(TestScenario item, boolean empty) {
                 super.updateItem(item, empty);
@@ -29,16 +29,47 @@ public class TestScenarioListViewManager {
                   setText(null);
                   setStyle("");
                 } else {
-                  setText(item.toString());
+                  String displayText =
+                      item.getScenarioName() + " - " + item.getScenarioDescription();
+                  setText(
+                      displayText.length() > 100
+                          ? displayText.substring(0, 97) + "..."
+                          : displayText);
                   setStyle(
                       "-fx-background-color: "
                           + (item.isCompleted() ? "lightgreen" : "salmon")
                           + ";");
                 }
               }
-            };
-          }
-        });
+            });
+
+    listView.setOnMouseClicked(this::handleMouseClick);
+  }
+
+  private void handleMouseClick(MouseEvent event) {
+    if (event.getButton() == MouseButton.PRIMARY && event.getClickCount() == 2) {
+      TestScenario selectedScenario = listView.getSelectionModel().getSelectedItem();
+      if (selectedScenario != null) {
+        showDetailsDialog(selectedScenario);
+      }
+    }
+  }
+
+  private void showDetailsDialog(TestScenario scenario) {
+    Alert alert = new Alert(Alert.AlertType.INFORMATION);
+    alert.setTitle("Test Scenario Details");
+    alert.setHeaderText(scenario.getScenarioName());
+    String content =
+        "Description: "
+            + scenario.getScenarioDescription()
+            + "\nRequest: "
+            + scenario.getRequest()
+            + "\nExpected Response: "
+            + scenario.getExpectedResponse()
+            + "\nCompleted: "
+            + (scenario.isCompleted() ? "Yes" : "No");
+    alert.setContentText(content);
+    alert.showAndWait();
   }
 
   public void addTestScenarios(List<TestScenario> scenarios) {
